aac04ecd4c829a28869f19cb6bf12ed42da65360
za0012 <zinazeek@gmail.com>
2025-09-11 15:02:04 +0900

    feat: 1. 게시글 타입 파일을 추가. sort는 2번 사용되므로 분리. 2. article은 향후 재사용 가능성이 있어 미리 분리. 3. Board.tsx에 기존에 데이터를 가져와 console로 호출하는 코드를 사용하였으나, 콘솔에 몇 백번씩 찍히는 상황 발생, useEffect를 통해 한 번만 호출하도록 수정 4. 리드미 내용 추가

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ky from "ky";
+import { Article, ArticleListResponse } from "@/types/board";
 
 const Board = () => {
-  const fetchData = async () => {
-    const data = await ky
-      .get(
-        "http://localhost:8080/api/articles?page=0&size=10&sortBy=createdAt&sortDir=desc"
-      )
-      .json();
-    console.log(data);
-  };
-  return <div>Board</div>;
+  const [data, setData] = useState<ArticleListResponse | null>(null);
+
+  useEffect(() => { // useEffect를 추가해서 첫 로딩 시에만 데이터가 불러와지도록 함,
+    const fetchData = async () => {
+      try {
+        const data = await ky
+          .get(
+            "http://localhost:8080/api/articles?page=0&size=10&sortBy=createdAt&sortDir=desc"
+          )
+          .json<ArticleListResponse>();
+        console.log(data);
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, data);
+  return (
+    <div>
+      {data && data?.content?.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+        </div>
+      ))}
+      {data?.map((item) => {
+        <div>{item.}</div>
+      })}
+    </div>
+  );
 };
 
 export default Board;
---
+++ frontend/src/types/board.ts
@@ -0,0 +1,41 @@
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  user_id: number;
+  createdAt: string;
+  updatedAt: string;
+  viewCount: number;
+}
+
+interface Sort {
+  // 한 번 쓰는 것이 아니라 여러 번 사용할 것 같아, 따로 빼서 interface로 정의
+  empty: boolean;
+  unsorted: boolean;
+  sorted: boolean;
+}
+
+export interface ArticleListResponse {
+  content: Article[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    unpaged: boolean;
+  };
+  size: number;
+  sort: Sort;
+  totalElements: number;
+  totalPages: number;
+}
+
+type ApiResponse = {
+  content: Article[];
+  // add other properties if needed
+};
