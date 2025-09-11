6b0313974696a06e73036266158dec32005bba7f
za0012 <zinazeek@gmail.com>
2025-09-11 17:44:29 +0900

    style: index.css 파일 추가 및 main.css 파일 삭제, About 페이지의 스타일 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/assets/css/index.css";
-import "@/assets/css/main.css";
 import App from "@/App";
 import reportWebVitals from "./reportWebVitals";
 
