078e89d7fcdbaa7281d4c7295f0d156f7d11feda
za0012 <zinazeek@gmail.com>
2025-09-16 15:26:20 +0900

    feat: Tailwind CSS 프리텐다드 추가 업데이트 경로에 타입스크립트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Pretendard Variable"', "ui-sans-serif", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
